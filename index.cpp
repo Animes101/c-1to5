f3b89cc886d70700823e7cf01fcb45c751fcb270
Animes <animesbarman101@gmail.com>
2025-10-30 11:05:30 +0600

    add all

--- index.cpp
+++ index.cpp
@@ -1,12 +1,23 @@
-#include<iostream>>
+#include <iostream>
 using namespace std;
 
+
+// int main()
+// {
+
+//     cout<<"hello word this is hery sinple" <<"haram kohor is very swite" << "good";
+//     cin.get();
+
+// }
+
+
+
+// 5 escape sequence / backslash character \a bell \b backspace \f form feed \n new line \r carriger return \t horizanta tab \v vertical tab \o null character \'  single quatoe \" double quate \\ backslash \? questionmark
 
 int main()
 {
 
-    cout<<"änimes barman is a c++ learner and good student";
-    return 0;
+    cout << " \" animes barman  \"  \n is a developer \? ";
 
-
+    cin.get();
 }
